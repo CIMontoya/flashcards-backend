3b0141be3fc2a051243691bf2f8c1b04cd8d2807
Cesar Montoya <cesarm@Cesars-MacBook-Pro.local>
2019-01-25 13:36:39 -0700

    added cors as a dependancy

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
+var cors = require('cors')
+
+app.use(cors())
 
 const env = 'development';
 const config = require('./knexfile.js')[env];
