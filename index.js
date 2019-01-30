db221243336a2e7322265dceee7ee254da71287c
Cesar Montoya <cesarm@Cesars-MacBook-Pro.local>
2019-01-30 15:12:06 -0700

    changes

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const dotenv = require(‘dotenv’).config()
 
+const env = process.env.NODE_ENV || 'development';
+const config = require('./knexfile.js')[env];
+const knex = require('knex')(config);
 
 
 app.use(cors())
@@ -12,9 +15,6 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const env = process.env.NODE_ENV || 'development';
-const config = require('./knexfile.js')[env];
-const knex = require('knex')(config);
 
 app.get('/', (req, res, next) => {
     knex.select('*').from('methods')
